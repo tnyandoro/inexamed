d157277cb1a7ec14fa8db5357842f43db4aaaf29
Clayton Siby <claytonsiby@gmail.com>
2022-07-30 19:48:53 +0200

    Configure sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Authentication from './components/Authentication';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Authentication />
     </div>
   );
 }
