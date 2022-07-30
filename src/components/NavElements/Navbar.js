45381bcab8f2ea4f827514cb6ad77960bd6162cf
Rumbie-Mudzie7 <rumbiemudzie@gmail.com>
2022-07-31 00:02:45 +0200

    Create navbar and style it

--- src/components/NavElements/Navbar.js
+++ src/components/NavElements/Navbar.js
@@ -1,16 +1,61 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
+import MainLogo from '../../assets/inexamed-logo.PNG';
 
-function Navbar() {
+function NavBar() {
   return (
     <Nav>
-        < a href=""></a> 
+      <Logo href="">
+        <img src={MainLogo} alt="Inexamed logo" width={100} height={100} />
+      </Logo>
+      <Menu>
+        <MenuLink href="">Drug Interactions</MenuLink>
+        <MenuLink href="">Interaction List</MenuLink>
+        <MenuLink href="">Pregnancy List</MenuLink>
+        <MenuLink href="">Patient Registration</MenuLink>
+        <MenuLink href="">Add Interactions</MenuLink>
+        <MenuLink href="">References</MenuLink>
+      </Menu>
     </Nav>
-  )
+
+  );
 }
 
 const Nav = styled.div`
+    padding: 0 2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+    flex-wrap: wrap;
+    background : grey;
+`;
+const Logo = styled.a`
 
-`
+`;
+const Menu = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+    position: relative;
 
-export default Navbar
+    @media (max-width: 768px) {
+        overflow: hidden;
+        flex-direction: column;
+        width: 100px;
+    }
+`;
+const MenuLink = styled.a`
+    padding: 1rem 2rem;
+    cursor: pointer;
+    align-items: center;
+    text-decoration: none;
+    color: rgb(83, 58, 99);
+    font-weight: bold;
+    transition: all 0.5s ease-in;
+    
+    &:hover {
+        color: #fff;  
+    }
+`;
+
+export default NavBar;
