aa13944a8534602a79826317f3d5b44a2c3624a7
Clayton Siby <claytonsiby@gmail.com>
2022-07-30 22:34:13 +0200

    Configure redux store for login and signup

---
+++ src/redux/login/loginActions.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import login from './loginTypes';
+
+const loginRequest = () => ({
+  type: login.LOGIN_REQUEST,
+});
+
+const loginSuccess = (token) => ({
+  type: login.LOGIN_SUCCESS,
+  payload: token,
+});
+
+const loginFailure = (error) => ({
+  type: login.LOGIN_FAILURE,
+  payload: error,
+});
+
+const requestToLogin = (data) => (dispatch) => {
+  dispatch(loginRequest());
+  axios.post('https://the-gentleman-api.herokuapp.com/auth/login', data)
+    .then((response) => {
+      dispatch(loginSuccess(response.data));
+    })
+    .catch((error) => dispatch(loginFailure(error.message)));
+};
+
+export default requestToLogin;
