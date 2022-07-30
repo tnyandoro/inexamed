33fc609b58bfa6cc64732f21f510e82f5f38deef
Rumbie-Mudzie7 <rumbiemudzie@gmail.com>
2022-07-30 22:14:04 +0200

    Created skeleton navbar function

---
+++ src/components/NavElements/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Navbar() {
+  return (
+    <Nav>
+        < a href=""></a> 
+    </Nav>
+  )
+}
+
+const Nav = styled.div`
+
+`
+
+export default Navbar
