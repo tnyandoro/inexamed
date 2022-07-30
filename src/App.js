55c6a6b728466a2934d928fe22ab153589975373
Rumbie-Mudzie7 <rumbiemudzie@gmail.com>
2022-07-30 22:12:31 +0200

    Installed styled-components dependency

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Authentication from './components/Authentication';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
       <Authentication />
       <Button>Bootstrap Button</Button>
     </div>
