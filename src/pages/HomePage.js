a93ccdfb4ee3e8af1a3647ad491caa0a1c9fd2b8
tendainy <tendai.nyandoro@platform45.com>
2022-08-01 14:31:30 +0200

    Add text and button to landing page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function HomePage() {
+function LandingPageButton() {
+  return (
+    <Link to="/about" class="nav-link">
+      <button class="btn btn-primary">
+        <span style={{ "font-size": "24px" }}>Click Me!</span>
+      </button>
+    </Link>
+  );
+}
+
+function LandingFrameMessage() {
   const style = {
-    "background-image": `url("assets/images/background.jpg")`,
+    margin: "auto",
+    padding: "10% 35% 10% 15%",
+    color: "white",
+  };
+  return (
+    <div style={style}>
+      <div style={{ "font-size": "96px" }}>Hello World!!</div>
+      <div style={{ "font-size": "36px" }}>
+        This is the landing page and here's some content. How much wood would a
+        woodchuck chuck if a woodchuck would chuck wood?
+      </div>{" "}
+      <br /> <LandingPageButton />{" "}
+    </div>
+  );
+}
+
+function LandingFrame() {
+  const style = {
+    "background-image": `url("images/background.jpg")`,
     "background-repeat": "no-repeat",
     "background-size": "cover",
     position: "absolute",
     height: "100%",
     width: "100%",
   };
-  return <div style={style}>HomePage</div>;
+  return (
+    <div style={style}>
+      <LandingFrameMessage />
+    </div>
+  );
 }
-
 function HomePage() {
   return (
     <div>
@@ -19,5 +51,4 @@ function HomePage() {
     </div>
   );
 }
-
 export default HomePage;
