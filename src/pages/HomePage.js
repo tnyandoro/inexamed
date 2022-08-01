9ee307aa89b53bd9c625782c138b0860684bf797
tendainy <tendai.nyandoro@platform45.com>
2022-08-01 13:57:00 +0200

    Add the kanding page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React from "react";
+
+function HomePage() {
+  const style = {
+    "background-image": `url("assets/images/background.jpg")`,
+    "background-repeat": "no-repeat",
+    "background-size": "cover",
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+  };
+  return <div style={style}>HomePage</div>;
+}
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <LandingFrame />
+    </div>
   );
 }
 
