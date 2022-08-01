12d6ab0c8ba97fd4bded616d04480f7aef405881
tendainy <tendai.nyandoro@platform45.com>
2022-08-01 15:26:16 +0200

    Add styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function LandingPageButton() {
   return (
     <Link to="/about" class="nav-link">
-      <button class="btn btn-primary">
-        <span style={{ "font-size": "24px" }}>SignUp!</span>
+      <button type="button" className="btn btn-primary">
+        <span style={{ 'font-size': '24px' }}>Sign Up!</span>
       </button>
     </Link>
   );
@@ -13,31 +13,34 @@ function LandingPageButton() {
 
 function LandingFrameMessage() {
   const style = {
-    margin: "auto",
-    padding: "10% 35% 10% 15%",
-    color: "white",
+    margin: 'auto',
+    padding: '10% 35% 10% 15%',
+    color: 'black',
   };
   return (
     <div style={style}>
-      <div style={{ "font-size": "96px" }}>Drug Interaction checker!</div>
-      <div style={{ "font-size": "36px" }}>
+      <div style={{ 'font-size': '76px' }}>Drug Interaction checker!</div>
+      <div style={{ 'font-size': '36px' }}>
         Analyze prescription and OTC drug interactions to determine which drug
         combinations your patients should avoid. Pregnant patients are
         especially important to consider.
-      </div>{" "}
-      <br /> <LandingPageButton />{" "}
+      </div>
+      {' '}
+      <br />
+      <LandingPageButton />
+      {' '}
     </div>
   );
 }
 
 function LandingFrame() {
   const style = {
-    "background-image": `url("images/background.jpg")`,
-    "background-repeat": "no-repeat",
-    "background-size": "cover",
-    position: "absolute",
-    height: "100%",
-    width: "100%",
+    'background-image': 'url("./background.jpg")',
+    'background-repeat': 'no-repeat',
+    'background-size': 'cover',
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
   };
   return (
     <div style={style}>
