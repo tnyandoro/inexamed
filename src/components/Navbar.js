d36451672cefaa35899259d5fa7ace25ba6ff065
tendainy <tendai.nyandoro@platform45.com>
2022-08-09 15:31:32 +0200

    Add changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -9,6 +13,36 @@ function Navbar() {
           INXAMED
           <i className="fa fa-medkit" />
         </Link>
+        <div aria-hidden="true" className="menu-icon" onClick={handleClick} onKeyDown={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Interactions
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Pregnancy
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Add Interactions
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              References
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
