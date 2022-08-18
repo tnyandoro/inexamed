2e47865ea619aafe9444c93c55fb73521a295823
tendainy <tendai.nyandoro@platform45.com>
2022-08-19 00:06:45 +0200

    Add API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
+import AppContextProvider from './store/appContextProvider';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(
+	<React.StrictMode>
+		<AppContextProvider>
+			<App />
+		</AppContextProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
+
+
+reportWebVitals();
